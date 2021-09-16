bdc9874ebc61c8db908ee0ae634025c01a642d8e
Erick Amezcua <erickamezcua12@gmail.com>
2021-09-16 12:27:35 -0600

    Init commit

---
+++ Assets/Scenes/Scripts/Move/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+  public float speed = 8f;
+  public float speedMultiplier = 1f;
+  //Change this variable for move
+  public Vector2 direction = Vector2.zero;
+  public new Rigidbody2D rigidbody { get; private set; }
+
+  private void Awake()
+  {
+    rigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  private void FixedUpdate()
+  {
+    Vector2 position = rigidbody.position;
+    Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+    rigidbody.MovePosition(position + translation);
+  }
+}
---
+++ Assets/Scenes/Scripts/Move/PlayerMoveInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class PlayerMoveInput : MonoBehaviour
+{
+  public Movement movement { get; private set; }
+
+  private void Awake()
+  {
+    movement = GetComponent<Movement>();
+  }
+
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.W))
+    {
+      movement.direction = Vector2.up;
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+      movement.direction = Vector2.down;
+    }
+    else if (Input.GetKey(KeyCode.A))
+    {
+      movement.direction = Vector2.left;
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      movement.direction = Vector2.right;
+    }
+    else
+    {
+      movement.direction = Vector2.zero;
+    }
+  }
+}
