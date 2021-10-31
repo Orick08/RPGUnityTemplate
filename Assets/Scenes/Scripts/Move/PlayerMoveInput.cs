caf6f27567efd8e19a18631ac0a61d02d1af7adb
Erick Amezcua <erickamezcua12@gmail.com>
2021-10-31 16:25:19 -0700

    Prevents movement with oposite input

--- Assets/Scenes/Scripts/Move/PlayerMoveInput.cs
+++ Assets/Scenes/Scripts/Move/PlayerMoveInput.cs
@@ -12,19 +12,19 @@ public class PlayerMoveInput : MonoBehaviour
 
   void Update()
   {
-    if (Input.GetKey(KeyCode.W))
+    if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
     {
       movement.direction = Vector2.up;
     }
-    else if (Input.GetKey(KeyCode.S))
+    else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
     {
       movement.direction = Vector2.down;
     }
-    else if (Input.GetKey(KeyCode.A))
+    else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
     {
       movement.direction = Vector2.left;
     }
-    else if (Input.GetKey(KeyCode.D))
+    else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
     {
       movement.direction = Vector2.right;
     }
