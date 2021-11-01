7df2e3f04c3d912943d8ec8c47839ab6292f3d45
Erick Amezcua <erickamezcua12@gmail.com>
2021-10-31 17:05:20 -0700

    Implements player animation

--- Assets/Scripts/Move/Movement.cs
+++ Assets/Scripts/Move/Movement.cs
@@ -8,14 +8,26 @@ public class Movement : MonoBehaviour
   //Change this variable for move
   public Vector2 direction = Vector2.zero;
   public new Rigidbody2D rigidbody { get; private set; }
+  private Animator animator;
 
   private void Awake()
   {
     rigidbody = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
   }
 
   private void FixedUpdate()
   {
+    if (direction == Vector2.zero)
+    {
+      animator.SetBool("isMoving", true);
+    }
+    else
+    {
+      animator.SetBool("isMoving", false);
+      animator.SetFloat("x", direction.x);
+      animator.SetFloat("y", direction.y);
+    }
     Vector2 position = rigidbody.position;
     Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
     rigidbody.MovePosition(position + translation);
